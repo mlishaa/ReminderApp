40c11461a6492648f5a5663997e28c93332b8e01
mac <mac@DESKTOP-T73Q40O>
2019-11-28 04:21:09 -0500

    first

---
+++ ReminderApp/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReminderApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+   
+    public partial class MainWindow : Window
+    {
+        private string connectionDB = ConfigurationManager.ConnectionStrings["ReminderApp.Properties.Settings.DBReminderConnectionString"].ConnectionString;
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateListView();
+        }
+
+        private void PopulateListView()
+        {
+            //List<string> mylist = new List<string>();
+            string command = @"select * from Reminders";
+            DataSet sd = new DataSet();
+            using(SqlConnection con=new SqlConnection(connectionDB))
+            {
+                SqlCommand cmd = new SqlCommand(command, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(sd, "short");
+                foreach(DataRow data in sd.Tables["short"].Rows)
+                {
+                    listShortDescription.Items.Add(data["Name"].ToString());
+
+                }
+                listShortDescription.SelectedValuePath = ".[Name]";
+             //   listShortDescription.DisplayMemberPath = ".[Name]";
+
+
+            }
+
+
+        }
+
+        private void listShortDescription_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            string getLong = listShortDescription.SelectedItem.ToString();
+
+            string command = @"select LongDescription from Reminders where Name like '" + getLong + "' ";
+            DataSet sd = new DataSet();
+            using (SqlConnection con = new SqlConnection(connectionDB))
+            {
+                SqlCommand cmd = new SqlCommand(command, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(sd, "short");
+                txtLongDescription.Text = sd.Tables["short"].Rows.ToString();
+            }
+        }
+    }
+}
---
+++ ReminderApp/NewReminderWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReminderApp
+{
+    /// <summary>
+    /// Interaction logic for NewReminderWindow.xaml
+    /// </summary>
+    public partial class NewReminderWindow : Window
+    {
+        private string connectionDB = ConfigurationManager.ConnectionStrings["ReminderApp.Properties.Settings.DBReminderConnectionString"].ConnectionString;
+
+        public NewReminderWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void btnSaveReminder_Click(object sender, RoutedEventArgs e)
+        {
+            //try {
+            //    if (CollectTableDate())
+            //    {
+            // string command = @"INSERT INTO Reminders(Name,LongDescription,DueDate) VALUES('" + txtShortDes.Text + @"','" + txtLongDes.Text + @"','" + dPDueDate.SelectedDate.Value + @"')";
+            //        SqlConnection con = new SqlConnection();
+            //          con.ConnectionString = ConfigurationManager.ConnectionStrings["ReminderApp.Properties.Settings.DBReminderConnectionString"].ConnectionString;
+
+            //            con.Open();
+            //            SqlCommand cmd = new SqlCommand();
+            //        cmd.CommandText = "INSERT INTO [dbo].[Reminders](Name,LongDescription,DueDate) VALUES(@sd,@ld,@d)";
+            //        cmd.Parameters.AddWithValue("@sd", txtShortDes.Text);
+            //        cmd.Parameters.AddWithValue("@ld", txtLongDes.Text);
+            //        cmd.Parameters.AddWithValue("@d", dPDueDate.SelectedDate.Value);
+            //            cmd.Connection = con;
+            //            int a=cmd.ExecuteNonQuery();
+            //            if (a == 1)
+            //            {
+            //                MessageBox.Show("yes");
+            //            }
+            //        con.Close();
+
+            //}
+
+
+            //}catch(Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            //}
+
+            //private bool CollectTableDate()
+            //{
+            //    if (string.IsNullOrEmpty(txtShortDes.Text)
+            //        || string.IsNullOrEmpty(txtLongDes.Text) ||
+            //        string.IsNullOrEmpty(dPDueDate.SelectedDate.ToString())){
+            //        return false;
+            //    } else
+            //        return true;
+            //}
+        }
+
+    }
+}
